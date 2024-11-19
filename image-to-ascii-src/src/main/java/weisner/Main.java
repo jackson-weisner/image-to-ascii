7ee4f21706458e34268119a421afcf3f7f537b26
jackson-weisner <jackson.weisner1@gmail.com>
2024-11-19 12:56:48 -0600

    Refactored files

--- image-to-ascii-src/src/main/java/weisner/Main.java
+++ image-to-ascii-src/src/main/java/weisner/Main.java
@@ -3,19 +3,21 @@ package weisner;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            if (args.length != 1) throw new IOException();
-            BufferedImage image = ImageIO.read(new File("./images/" + args[0]));
+            System.out.println("Enter a image file name:");
+            Scanner scanner = new Scanner(System.in);
+            BufferedImage image = ImageIO.read(new File("./images/" + scanner.nextLine()));
             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("./output/out.txt"));
 
             // loop over the image and convert each pixel to gray scale then ascii
             for (int i = 0; i < image.getHeight(); i++) {
                 for (int j = 0; j < image.getWidth(); j++) {
-                    RGB values = new RGB(image.getRGB(j, i));
-                    out.write(values.toGrayScale().toAscii().getBytes());
+                    Pixel pixel = new Pixel(image.getRGB(j, i));
+                    out.write(pixel.toGrayScale().toAscii().getBytes());
                 }
                 out.write("\n".getBytes());
             }
